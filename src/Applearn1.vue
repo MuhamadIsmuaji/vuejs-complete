6c2bccf3f122789021564b52e2e5d0c1ef072a06
MuhamadPrajitno <muhamadismuaji@gmail.com>
2017-09-02 08:01:51 +0700

    Learn about slots

---
+++ src/Applearn1.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<app-quote>
+					<h2 slot="title">{{ quoteTitle }}</h2>
+					<p>A wonderful quote</p>
+				</app-quote>
+			</div>		
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	import Quote from './components/Learn1/Quote.vue'
+
+	export default {
+		data: function() {
+			return {
+				quoteTitle: 'The Quote !',
+			};
+		},
+		components: {
+			appQuote: Quote, 
+		}
+	}
+</script>
+
+<style type="text/css" scoped>
+	
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Applearn1 from './Applearn1.vue'
+// import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+	el: '#app',
+	render: h => h(Applearn1)
 })
+
+// new Vue({
+// 	el: '#app',
+// 	render: h => h(App)
+// })
