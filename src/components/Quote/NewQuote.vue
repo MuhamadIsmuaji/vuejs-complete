299f85ab3ac495bfe937a90ba92c1d818a5386d4
MuhamadPrajitno <muhamadismuaji@gmail.com>
2017-09-02 09:58:38 +0700

    Simple project quotes

---
+++ src/components/Quote/NewQuote.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="row">
+		<div class="col-md-12">
+			<form>
+				<div class="form-group">
+					<label>Quote</label>
+					<textarea class="form-control" rows="3" v-model="quote"></textarea>
+				</div>
+				<div class="form-group">
+					<button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		data: function() {
+			return {
+				quote: '',
+			}
+		},
+		methods: {
+			createNew() {
+				this.$emit('quoteAdded', this.quote);
+				this.quote = '';
+			}
+		}
+	}
+</script>
--- src/main.js
+++ src/main.js
@@ -6,19 +6,20 @@ import Vue from 'vue'
 // learn about dynamic components
 import Applearn2 from './Applearn2.vue'
 
-// import App from './App.vue'
+// learn about quote project
+import App from './App.vue'
 
-new Vue({
-	el: '#app',
-	render: h => h(Applearn2)
-})
+// new Vue({
+// 	el: '#app',
+// 	render: h => h(Applearn2)
+// })
 
 // new Vue({
 // 	el: '#app',
 // 	render: h => h(Applearn1)
 // })
 
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(App)
-// })
+new Vue({
+	el: '#app',
+	render: h => h(App)
+})
