9ae64cd16958fcda8c36a356494066d306bf9815
MuhamadPrajitno <muhamadismuaji@gmail.com>
2017-09-02 08:25:04 +0700

    Learn about dynamic component

---
+++ src/Applearn2.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="row">
+					<button class="btn btn-primary" @click="selectedComponent = 'appMain'">Main</button>
+					<button class="btn btn-primary" @click="selectedComponent = 'appAbout'">About</button>
+					<button class="btn btn-primary" @click="selectedComponent = 'appOther'">Other</button>
+					<hr>
+					<p>Selected components: {{ selectedComponent }}</p>
+				</div>
+				<hr>
+				<div class="row">
+					<!-- will destroyed when change the component -->
+					<!-- <component :is="selectedComponent"></component> -->
+					
+					<!-- will activated and deactivated when change the component -->
+					<keep-alive>
+						<component :is="selectedComponent"></component>
+					</keep-alive>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	import Main from './components/Learn2/Main.vue'
+	import About from './components/Learn2/About.vue'
+	import Other from './components/Learn2/Other.vue'
+
+	export default {
+		data: function() {
+			return {
+				selectedComponent: 'appMain',
+			}
+		},
+		components: {
+			appMain: Main,
+			appAbout: About,
+			appOther: Other,
+		}
+	}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
-import Applearn1 from './Applearn1.vue'
+
+// learn about slots
+// import Applearn1 from './Applearn1.vue'
+
+// learn about dynamic components
+import Applearn2 from './Applearn2.vue'
+
 // import App from './App.vue'
 
 new Vue({
 	el: '#app',
-	render: h => h(Applearn1)
+	render: h => h(Applearn2)
 })
+
+// new Vue({
+// 	el: '#app',
+// 	render: h => h(Applearn1)
+// })
 
 // new Vue({
 // 	el: '#app',
