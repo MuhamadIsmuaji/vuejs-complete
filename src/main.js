9b6d71cb61407a084dcb074dcedd62c939944627
MuhamadPrajitno <muhamadismuaji@gmail.com>
2017-09-10 10:18:50 +0700

    using and creating directives done

--- src/main.js
+++ src/main.js
@@ -1,35 +1,6 @@
 import Vue from 'vue'
 
-// learn about slots
-// import Applearn1 from './Applearn1.vue'
-
-// learn about dynamic components
-// import Applearn2 from './Applearn2.vue'
-
-// learn about forms
-// import Learnform from './Learnform.vue'
-
-// learn about directive
 import Learndirective from './Learndirective.vue'
-
-// learn about quote project
-// import App from './App.vue'
-
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(Applearn2)
-// })
-
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(Applearn1)
-// })
-
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(Learnform)
-// })
-
 
 /*
 Learn directive notes
@@ -101,8 +72,3 @@ new Vue({
 	el: '#app',
 	render: h => h(Learndirective)
 })
-
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(App)
-// })
