a7540e2c2ba43c2b985ec3629372e1c014235380
MuhamadPrajitno <muhamadismuaji@gmail.com>
2017-09-13 08:36:11 +0700

    course done

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,115 @@
 <template>
 	<div class="container">
-		<app-header-quote :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header-quote>
-		<app-new-quote @quoteAdded="quoteAdd"></app-new-quote>
-		<app-quote-grid :quotes="quotes" @quoteDeleted="quoteDelete"></app-quote-grid>
-		<div class="row">
-			<div class="col-md-12">
-				<div class="alert alert-info">
-					<p class="text-center">Click quote to delete it.</p>
+		<div class="row row-justify">
+			<div class="col-md-8">
+				<h2 class="text-center">Connecting To Server Via HTTP With Vue Resource</h2>
+				<hr>
+				<div class="form-group">
+					<label>Username</label>
+					<input class="form-control" type="text" v-model="user.username">
 				</div>
+				<div class="form-group">
+					<label>Email</label>
+					<input class="form-control" type="text" v-model="user.email">
+				</div>
+				<button class="btn btn-info" @click="submit">Submit</button>
+				<hr>
+				<input class="form-control" type="text" v-model="node"> <br>
+				<button class="btn btn-info" @click="fetch">Get Data</button>
+				<hr>
+				<ul class="list-group">
+					<li class="list-group-item" v-for="u in users">
+						{{ u.username }} - {{ u.email }}
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script type="text/javascript">
-	import QuoteGrid from './components/Quote/QuoteGrid.vue'
-	import NewQuote from './components/Quote/NewQuote.vue'
-	import HeaderQuote from './components/Quote/HeaderQuote.vue'
-
 	export default {
-		data: function() {
+		data() {
 			return {
-				quotes: [
-					'The first quotes',
-				],
-				maxQuotes: 10,
+				user: {
+					username: '',
+					email: '',
+				},
+				users: [],
+				resource: {},
+				node: 'data',
 			}
 		},
 		methods: {
-			quoteAdd(quote) {
-				if (this.quotes.length >= this.maxQuotes) {
-					return alert('Delete quote first!')
-				}
-				this.quotes.push(quote);
+			submit() {
+				// console.log(this.user);
+				// this.$http.post('data.json', this.user)
+				// 	.then(response => {
+				// 		console.log(response);
+				// 	}, error => {
+				// 		console.log(error);
+				// 	});
+				
+				// this.resource.save({},this.user);
+				
+				// custom resource
+				this.resource.saveAlt(this.user);
 			},
-			quoteDelete(index) {
-				this.quotes.splice(index, 1);
+			fetch() {
+				// this.$http.get('data.json')
+				// 	.then(response => {
+				// 		// console.log(response.json());
+				// 		return response.json();
+				// 	})
+				// 	.then(data => {
+				// 		// console.log(data)
+				// 		const resArray = [];
+
+				// 		for (let key in data) {
+				// 			// console.log(data[key]);
+				// 			resArray.push(data[key])
+				// 		}
+
+				// 		this.users = resArray;
+
+				// 	});
+				
+				// custom resource
+				this.resource.getData({node: this.node})
+					.then(response => {
+						// console.log(response.json());
+						return response.json();
+					})
+					.then(data => {
+						// console.log(data)
+						const resArray = [];
+
+						for (let key in data) {
+							// console.log(data[key]);
+							resArray.push(data[key])
+						}
+
+						this.users = resArray;
+
+					});
+				// this.resource.getData({node: this.node, another: this.another});
 			}
 		},
-		components: {
-			appQuoteGrid: QuoteGrid,
-			appNewQuote: NewQuote,
-			appHeaderQuote: HeaderQuote,
+		created() {
+			// console.log('created');
+			
+			const customAction = {
+				saveAlt: {method: 'POST', url: 'alternative.json'},
+				getData: {method: 'GET'}
+			};
+
+			// $ sign indicated come from root resource
+			// this.resource = this.$resource('data.json');
+			
+			// custom resource
+			// this.resource = this.$resource('data.json', {}, customAction);
+			this.resource = this.$resource('{node}.json', {}, customAction);
+			// this.resource = this.$resource('{node}/{another}.json', {}, customAction);
 		}
 	}
 </script>
-
-<style type="text/css">
-	.row {
-		margin-top: 15px;
-	}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,109 +1,30 @@
 import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
 
-// dummy comment
-// learn about slots
-// import Applearn1 from './Applearn1.vue'
+Vue.use(VueResource);
 
-// learn about dynamic components
-// import Applearn2 from './Applearn2.vue'
+Vue.http.options.root = 'https://vuejs-http-82ef7.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+	console.log(request);
 
-// learn about forms
-// import Learnform from './Learnform.vue'
-
-// learn about directive
-import Learndirective from './Learndirective.vue'
-
-// learn about quote project
-// import App from './App.vue'
-
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(Applearn2)
-// })
-
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(Applearn1)
-// })
-
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(Learnform)
-// })
-
-
-/*
-Learn directive notes
-
-Directive hooks
-
-bind(el, binding, vnode) => once directive is attached
-inserted(el, binding, vnode) => inserted in parent node
-updated(el, binding, vnode, oldVnode) => Once component is updated (without children)
-componentUpdated(el, binding, vnode, oldVnode) => Once component is updated (with children)
-unbind(el, binding, vnode) => once directive is removed 
-
-el => refers to directive element attach on
-binding => refers to way directive setup, which argument may pass it, which modifier may pass to it
-vnode => node to virtual dom
-oldVnode => old node in virtual dom
- */
-
-// simple custom directive
-Vue.directive('highlight', {
-	bind(el, binding, vnode) {
-		el.style.backgroundColor = 'green';
+	if (request.method == 'POST') {
+		request.method = 'PUT';
 	}
-})
 
-// custom directive with argument
-Vue.directive('highlight2', {
-	bind(el, binding, vnode) {
-		if (binding.arg == 'background') {
-			el.style.backgroundColor = binding.value;	
-		} else {
-			el.style.color = binding.value;
-		}
-	}
-});
-
-// custom directive with modifier
-Vue.directive('highlight3', {
-	bind(el, binding, vnode) {
-		var delay = 0;
-
-		if (binding.modifiers['delayed']) {
-			delay = 3000;
-		}
-
-		setTimeout(() => { // ES6
-			if (binding.arg == 'background') {
-				el.style.backgroundColor = binding.value;	
-			} else {
-				el.style.color = binding.value;
+	// next();
+	
+	// intercepting response
+	next(response => {
+		response.json = () => {
+			return {
+				messages: response.body
 			}
-		}, delay);
-	}
-});
-
-/*
-// binding.value is value after uquation mark
-		// el.style.backgroundColor = binding.value;
-		
-		// custom directive with argument
-		if (binding.arg == 'background') {
-			el.style.backgroundColor = binding.value;	
-		} else {
-			el.style.color = binding.value;
 		}
- */
+	});
+});
 
 new Vue({
 	el: '#app',
-	render: h => h(Learndirective)
-})
-
-// new Vue({
-// 	el: '#app',
-// 	render: h => h(App)
-// })
+	render: h => h(App)
+});
